23c3406a9db718e785ef92219fcc97b5de01991f
Kaif2000 <121150201+Kaif2000@users.noreply.github.com>
2022-12-21 12:49:35 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         system.out.println("");
+        //this is my comment by kaif
 
     }
 }
